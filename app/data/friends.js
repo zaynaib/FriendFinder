6f3a17ad9e4766eddeeb24dbebd35b0bff1039aa
Zaynaib <zaynaibg@gmail.com>
2017-10-29 15:22:23 -0500

    changed friend images and added css.

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,7 +5,7 @@
 var friends = [
 {
   "name":"Ahmed",
-  "photo":"http://18216-presscdn-0-94.pagely.netdna-cdn.com/wp-content/uploads/2017/07/beaut.jpg",
+  "photo":"https://vignette.wikia.nocookie.net/spongebob/images/8/89/Drifter.png/revision/latest?cb=20150728004136",
   "scores":[
       5,
       1,
@@ -37,7 +37,7 @@ var friends = [
 },
 {
   "name":"Sally",
-  "photo":"http://18216-presscdn-0-94.pagely.netdna-cdn.com/wp-content/uploads/2017/07/beaut.jpg",
+  "photo":"http://vignette1.wikia.nocookie.net/spongebob/images/0/0d/SpongeBob_SquarePants_Mrs._Puff_Character_Image_Nickelodeon_Painted_Version.png/revision/latest?cb=20151225034239",
   "scores":[
       6,
       3,
@@ -53,7 +53,7 @@ var friends = [
 },
 {
   "name":"Peter",
-  "photo":"http://18216-presscdn-0-94.pagely.netdna-cdn.com/wp-content/uploads/2017/07/beaut.jpg",
+  "photo":"http://www.behindthevoiceactors.com/_img/chars/squidward-tentacles-spongebob-squarepants-2.6.jpg",
   "scores":[
       3,
       2,
@@ -69,7 +69,7 @@ var friends = [
 },
 {
   "name":"Tony",
-  "photo":"http://18216-presscdn-0-94.pagely.netdna-cdn.com/wp-content/uploads/2017/07/beaut.jpg",
+  "photo":"https://vignette2.wikia.nocookie.net/cartoons/images/1/1c/Sandy_Cheeks_w-suit.png/revision/latest?cb=20081223021521",
   "scores":[
       4,
       2,
